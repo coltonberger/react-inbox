3fa516aafefedac8469c6d2394414458bc73c36e
Colton Berger <coltonberger@optonline.net>
2018-07-22 09:45:21 -0700

    message display correctly displayed.

--- src/components/MessageComponent.js
+++ src/components/MessageComponent.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react'
 
-const Message = (props) => {
+const Message = ({message}) => {
+  let labelList = message.labels.map((label, index) => {
+    return (
+      <span className="label label-warning">{label}</span>
+    )
+  })
+
   return (
-    <div className="row message read">
+    <div className={`row message read ${message.read ? 'read' : 'unread'} ${message.selected ? 'selected' : ''}`}>
       <div className="col-xs-1">
         <div className="row">
           <div className="col-xs-2">
-            <input type="checkbox" />
+            <input type="checkbox"checked={message.selected}/>
           </div>
           <div className="col-xs-2">
-            <i className="star fa fa-star"></i>
+            <i className={`star fa fa-star${message.starred ? '' : '-o'}`}></i>
           </div>
         </div>
       </div>
       <div className="col-xs-11">
-        <span className="label label-warning">dev</span>
-        <span className="label label-warning">gschool</span>
+        {labelList}
         <a href="#">
           Here is some message text that has a bunch of stuff
         </a>
